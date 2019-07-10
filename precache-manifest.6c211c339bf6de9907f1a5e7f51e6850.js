dfedd30a8013df5c1d5c0a23e6a6cc95d68e26c6
1690133293@qq.com <1690133293@qq.com>
2019-07-10 14:41:48 +0800

    Updates

---
+++ precache-manifest.6c211c339bf6de9907f1a5e7f51e6850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2d0ea85f86bd40a4a7e3bff93bffa0",
+    "url": "/super/index.html"
+  },
+  {
+    "revision": "6d96eb32265e226cc5c0",
+    "url": "/super/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "cbfa845e744572968061",
+    "url": "/super/static/js/2.a153ca48.chunk.js"
+  },
+  {
+    "revision": "6d96eb32265e226cc5c0",
+    "url": "/super/static/js/main.b62eb1c0.chunk.js"
+  },
+  {
+    "revision": "a0a7f4cab42464212055",
+    "url": "/super/static/js/runtime~main.396d02de.js"
+  }
+]);
